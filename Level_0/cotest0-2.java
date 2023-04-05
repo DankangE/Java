a9047c463228a9bb634230efdaa248580ad1dac2
DankangE <ekffy99@gmail.com>
2023-04-05 17:36:10 +0900

    Java_day1

---
+++ Level_0/cotest0-2.java
@@ -0,0 +1,18 @@
+package Level_0;
+class Nextnumber {
+
+    // 등차수열이면 common[1] - common[0]를 마지막 값에 더하기
+    // 등비수열이면 common[1] / common[0]를 마지막 값에 곱하기
+    public static int main(int[] common) {
+        int number = 0;
+        if(common[1] - common[0] == common[2] - common[1])      // 등차수열인지 확인
+            {
+                number = common[common.length-1] + (common[1] - common[0]);
+            }
+        else
+            {
+                number = common[common.length-1] * common[1] / common[0];
+            }
+        return number;
+    }
+}
---
+++ Level_0/cotest0-3.java
@@ -0,0 +1,17 @@
+package Level_0;
+class continuity {
+    public int[] TotalNum(int num, int total) {
+        int[] number = new int[num];
+        
+        // 연속된 수들의 가운데 숫자 = total / num
+
+        int sum = num * (1 + num) / 2;  // (1부터 3까지의 합 = 6) if(num == 4) : sum = 10 
+        int start_num = (total - sum) / num;  // 연속된 수의 시작점
+
+        for(int i=1; i<num+1; i++){
+                number[i - 1] = i + start_num;  // 시작점 + 1씩 늘려가면서 저장
+        }
+
+        return number;
+    }
+}
