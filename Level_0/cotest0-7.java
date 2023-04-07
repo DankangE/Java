897c2d23820cb4b69e19c550f3260cd88bfe74a0
DankangE <ekffy99@gmail.com>
2023-04-07 21:10:24 +0900

    Java_day3

---
+++ Level_0/cotest0-7.java
@@ -0,0 +1,38 @@
+package Level_0;
+
+import java.util.Arrays;
+// Arrays.toString(int배열) : 문자열 배열로 변환
+// equals() 메서드를 사용하여 특정 값이랑 비교
+class number7 {
+    public int solution(int[] array) {
+        int answer = 0;
+        String str = Arrays.toString(array);
+        for(int i=0; i<str.length(); i++ ){   // str 길이(문자열 길이)
+            if(str.charAt(i) == '7'){        // charAt() 함수는 문자열에서 특정 인덱스에 위치하는 유니코드 단일문자를 반환
+                answer++;
+            }
+            // if(str.equals("7"))   // 이 if문은 전체에서 7을 찾으므로 X
+            // {answer++;}
+        }
+
+        return answer;
+    }
+}
+
+// 다른 사람 풀이
+
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public int solution(int[] array) {
+//         return (int) Arrays.stream(
+//                         Arrays.stream(array)        // 스트림 생성
+//                                 .mapToObj(String::valueOf)     // 기본형 특화 스트림을 Stream으로 변환해주는 메서드
+//                                 .collect(Collectors.joining())   // 스트림에서 작업한 결과를 하나의 스트링으로 이어 붙일 수 있습니다.
+//                                 .split("")
+//                 )
+//                 .filter(s -> s.equals("7"))   // 필터(filter)은 스트림 내 요소들을 하나씩 평가해서 걸러내는 작업
+//                 .count();
+//     }
+// }
---
+++ Level_0/cotest0-8.java
@@ -0,0 +1,30 @@
+package Level_0;
+
+import java.util.*;
+
+class StringSort {
+    public String solution(String my_string) {
+        String answer = "";
+
+        my_string = my_string.toLowerCase(); // 대문자 -> 소문자
+        char[] charArr = my_string.toCharArray(); // String to Char Array
+        Arrays.sort(charArr);
+        String result = new String(charArr); // 또는 String.valueOf(charArr);
+
+        answer = result;
+
+        return answer;
+    }
+}
+
+
+// 다른 사람 풀이
+
+// import java.util.*;
+// class Solution {
+//     public String solution(String my_string) {
+//         char[] c = my_string.toLowerCase().toCharArray();
+//         Arrays.sort(c);
+//         return new String(c);
+//     }
+// }
