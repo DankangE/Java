ed85f27f76664355bb0fc7e9bec23f215c60a595
DankangE <ekffy99@gmail.com>
2023-04-08 23:20:50 +0900

    Java_day4

---
+++ Level_0/cotest0-10.java
@@ -0,0 +1,19 @@
+package Level_0;
+
+class cotest {
+    public int solution(int n) {
+        int answer = 2;
+        int cnt=0;
+
+        for(int i=1; i<1001; i++)
+        {
+            if(n == i*i){
+                cnt++;
+            }
+        }
+        if(cnt == 1){
+            answer = 1;
+        }
+        return answer;
+    }
+}
