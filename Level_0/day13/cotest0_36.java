39e2432cbcf263f9d342a8a2796ab8a6fcd1e009
YodanKang <ekffy99@gmail.com>
2023-04-30 13:54:41 +0900

    Java_Level0_day13

---
+++ Level_0/day13/cotest0_36.java
@@ -0,0 +1,10 @@
+package Level_0.day13;
+
+// 대문자로 바꾸기 https://school.programmers.co.kr/learn/courses/30/lessons/181877
+public class cotest0_36 {
+    public String solution(String myString) {
+        String answer = myString.toUpperCase();
+        return answer;
+    }
+    
+}
